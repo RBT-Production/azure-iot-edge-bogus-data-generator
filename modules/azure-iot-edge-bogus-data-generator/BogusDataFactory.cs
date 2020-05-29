4007a0fc242dc3797852fbcb9576103da297c5dd
alavrinenko <alavrinenko@veepee.com>
2020-05-29 18:43:40 +0300

    generator for esp system - motor load improved

--- modules/azure-iot-edge-bogus-data-generator/BogusDataFactory.cs
+++ modules/azure-iot-edge-bogus-data-generator/BogusDataFactory.cs
@@ -24,77 +24,10 @@ namespace IoTEdgeBogusDataGenerator
                 .StrictMode(true)
                 //Id is deterministic
                 .RuleFor(o => o.Id, f => counter)
-                //Generate Random value for Speed
-                .RuleFor(o => o.MotorLoad, f => f.Random.Number(80, 90))
-                //Generate Random value for CurrentLocation
-                .RuleFor(o => o.CurrentLocation, (f,o) => GetFakeCurrentLocation(f, o))
-                //Generate Random value for Destination
-                .RuleFor(o => o.Destination, (f,o) => GetFakeDestination(f, o.CurrentLocation))
-                //Generate Fake Ip Address
-                .RuleFor(o => o.Ip, f => f.Internet.Ip())
-                //Get Fake Items
-                .RuleFor(o => o.Token, f => f.Random.AlphaNumeric(10))
-                //Stamp with the current time
-                .RuleFor(o => o.Timestamp, f => DateTime.UtcNow);
-
-            return vehicleData.Generate();
-        }
-
-        public static string GetFakeCurrentLocation(Faker f, EspSystemData o)
-        {
-            //Latitude range( 49.95- 67.5) Longitude range( 25.10 – 135.05) 
-            double LatitudeMin = 49.95;
-            double LatitudeMax = 67.5;
-            double LongitudeMin = 25.10;
-            double LongitudeMax = 135.05;
-
-            double CurrentLatitude = 0;
-            double CurrentLongitude = 0;
-
-            //Introduce bias to approximately 1/10 samples
-            if(o.Id % 10 == 0)
-            {
-                //Moscow Coordinates
-                var CommonLatitude = 55.75;
-                var CommonLongitude = 37.61;
-
-                var LatitudeRange = 1;
-                var LongitudeRange = 1;
-
-                CurrentLatitude = LatitudeRange * f.Random.Double() + CommonLatitude;
-                CurrentLongitude = LongitudeRange * f.Random.Double() + CommonLongitude;
-
-                o.MotorLoad = f.Random.Number(0,25);
-                
-                Console.WriteLine("Correlation Forced");
-            }
-            else
-            {
-                var LatitudeRange = LatitudeMax - LatitudeMin;
-                var LongitudeRange = LongitudeMax - LongitudeMin;
-
-                CurrentLatitude = LatitudeRange * f.Random.Double() + LatitudeMin;
-                CurrentLongitude = LongitudeRange * f.Random.Double() + LongitudeMin;
-            }
+                //Generate Random value for motor load
+                .RuleFor(o => o.MotorLoad, f => f.Random.Number(80, 90));
             
-            return String.Format("{0:0.00},{1:0.00}", CurrentLatitude, CurrentLongitude);            
-
-        }
-        public static string GetFakeDestination(Faker f, string currentLocation)
-        {
-
-            var CurrentLocation = currentLocation.Split(',');
-            double CurrentLatitude = Convert.ToDouble(CurrentLocation[0]);
-            double CurrentLongitude = Convert.ToDouble(CurrentLocation[1]);
-
-            int DistanceMin = 10;
-            int DistanceMax = 25;
-
-            double DestinationLatitude = CurrentLatitude + f.Random.Int(DistanceMin, DistanceMax);
-            double DestinationLongitude = CurrentLongitude + f.Random.Int(DistanceMin, DistanceMax);
-
-            return String.Format("{0:0.00},{1:0.00}", DestinationLatitude, DestinationLongitude);
-
+            return vehicleData.Generate();
         }
     }
 }
--- modules/azure-iot-edge-bogus-data-generator/Models/EspSystemData.cs
+++ modules/azure-iot-edge-bogus-data-generator/Models/EspSystemData.cs
@@ -6,11 +6,6 @@ namespace IoTEdgeBogusDataGenerator.Models
    {
       public int Id { get; set; }
       public int MotorLoad { get; set; } 
-      public string CurrentLocation { get; set; }
-      public string Destination { get; set; }
-      public string Ip { get; set; }
-      public string Token { get; set; }
-      public DateTime Timestamp { get; set; } 
-   }
 
+   }
 }
